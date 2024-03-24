48c61f65ee6ecb8827f106d9c24a6d2483a23fb8
iSayU <103714465+nguyenvinhhuy123@users.noreply.github.com>
2024-03-24 16:50:51 +0700

    feat: debugger decorator and implementation of visitProgram

--- assignment3/src/test/CheckerSuite.py
+++ assignment3/src/test/CheckerSuite.py
@@ -1,11 +1,14 @@
 import unittest
 from TestUtils import TestChecker
 from AST import *
-
+from main.zcode.checker.StaticError import *
+from main.zcode.utils.AST import *
 
 class CheckerSuite(unittest.TestCase):
     def test_no_entry_point(self):
         input = """number a
+        number a
+        func main() return 1
         """
-        expect = "No Entry Point"
+        expect = str(Redeclared(Variable(), Id("a")))
         self.assertTrue(TestChecker.test(input, expect, 400))
